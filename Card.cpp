9ec7974ec9df32f9bc6b0acce5a252bbf5504069
AndrewChmutov <ulman.andre@gmail.com>
2021-02-02 20:52:45 +0200

    Card only

--- Card.cpp
+++ Card.cpp
@@ -1,5 +1,59 @@
-//
-// Created by uzer on 02.02.2021.
-//
-
 #include "Card.h"
+
+
+
+    Card::Card()
+    {
+        c_m = TREF;
+        c_n = SIX;
+    }
+
+    void Card::setMast(Mast m)
+    {
+        c_m = m;
+    }
+    Mast Card::getMast()
+    {
+        return c_m;
+    }
+
+    void Card::setNumber(Number n)
+    {
+        c_n = n;
+    }
+    Number Card::getNumber()
+    {
+        return c_n;
+    }
+
+
+    Card& Card::operator=(Card other)
+    {
+        this->c_m = other.getMast();
+
+        this->c_n = other.getNumber();
+
+        return (*this);
+    }
+
+    bool Card::compare(const Card& other, const Mast& k)
+    {
+        if(this->c_m == k){
+            if(other.c_m == k){
+                return (this->c_n > other.c_n);
+            }
+            else{
+                return true;
+            }
+        }
+        else{
+            if(other.c_m == k){
+                return false;
+            }
+            else{
+                return ((this->c_n > other.c_n) && (this->c_m == other.c_m));
+            }
+        }
+    }
+
+
--- Card.h
+++ Card.h
@@ -1,14 +1,33 @@
-//
-// Created by uzer on 02.02.2021.
-//
-
 #ifndef CARDS_CARD_H
 #define CARDS_CARD_H
 
-
+#include "global.h"
 class Card {
+protected:
+    Mast c_m;
+    Number c_n;
+
+public:
+
+    Card();
+
+    Card(Mast m, Number n) : c_m(m), c_n(n){};
+
+    void setMast(Mast m);
+
+    Mast getMast();
+
+    void setNumber(Number n);
+
+    Number getNumber();
+
+
+    Card& operator=(Card other);
+
+    bool compare(const Card& other, const Mast& k);
 
 };
+
 
 
 #endif //CARDS_CARD_H
